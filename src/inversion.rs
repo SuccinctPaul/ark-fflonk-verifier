8f156bca5211d3132e28fb6afce4620d5aae25f5
Paul Cheng <chengyuejia@foxmail.com>
2024-05-31 15:08:56 +0800

    feat: add ...

---
+++ src/inversion.rs
@@ -0,0 +1,298 @@
+use crate::{get_domain_size, get_omegas, get_proof};
+use ark_bn254::{Fr, FrParameters};
+use ark_ff::{Fp256, One, Zero};
+use std::ops::{Add, Mul, Sub};
+use std::str::FromStr;
+
+pub struct LISValues {
+    pub li_s0_inv: [Fp256<FrParameters>; 8],
+    pub li_s1_inv: [Fp256<FrParameters>; 4],
+    pub li_s2_inv: [Fp256<FrParameters>; 6],
+}
+
+pub fn calculateInversions(
+    y: Fp256<FrParameters>,
+    xi: Fp256<FrParameters>,
+    zhInv: Fp256<FrParameters>,
+    h0w8: Vec<Fp256<FrParameters>>,
+    h1w4: Vec<Fp256<FrParameters>>,
+    h2w3: Vec<Fp256<FrParameters>>,
+    h3w3: Vec<Fp256<FrParameters>>,
+) -> (
+    Fp256<FrParameters>,
+    LISValues,
+    Fp256<FrParameters>,
+    Fp256<FrParameters>,
+) {
+    // 1. compute den_h1 base
+    let mut w = y
+        .sub(h1w4[0])
+        .mul(y.sub(h1w4[1]).mul(y.sub(h1w4[2]).mul(y.sub(h1w4[3]))));
+    // println!("w: {}", (w));
+
+    let denH1 = w.clone();
+
+    w = y.sub(h2w3[0]).mul(
+        y.sub(h2w3[1])
+            .mul(y.sub(h2w3[2]))
+            .mul(y.sub(h3w3[0]).mul(y.sub(h3w3[1]).mul(y.sub(h3w3[2])))),
+    );
+
+    // println!("w: {}", (w));
+
+    let denH2 = w.clone();
+
+    let mut li_s0_inv = computeLiS0(y, h0w8);
+
+    let mut li_s1_inv = computeLiS1(y, h1w4);
+
+    let mut li_s2_inv = computeLiS2(y, xi, h2w3, h3w3);
+    // println!()
+
+    w = Fr::from_str("1").unwrap();
+
+    let mut eval_l1 = get_domain_size().mul(xi.sub(w));
+
+    // println!("eval_l1: {}", eval_l1);
+
+    let invser_arr_resp = inverseArray(
+        denH1,
+        denH2,
+        zhInv,
+        li_s0_inv,
+        li_s1_inv,
+        li_s2_inv,
+        &mut eval_l1,
+    );
+
+    (
+        eval_l1,
+        invser_arr_resp.0,
+        invser_arr_resp.1,
+        invser_arr_resp.2,
+    )
+}
+
+pub fn computeLiS0(
+    y: Fp256<FrParameters>,
+    h0w8: Vec<Fp256<FrParameters>>,
+) -> [Fp256<FrParameters>; 8] {
+    let root0 = h0w8[0];
+
+    let mut den1 = Fr::from_str("1").unwrap();
+    den1 = den1
+        .mul(root0)
+        .mul(root0)
+        .mul(root0)
+        .mul(root0)
+        .mul(root0)
+        .mul(root0);
+
+    // println!("den1: {}", den1);
+
+    den1 = den1.mul(Fr::from_str("8").unwrap());
+
+    let mut den2;
+    let mut den3;
+
+    let mut li_s0_inv: [Fp256<FrParameters>; 8] = [Fr::zero(); 8];
+
+    let q = Fr::from_str(
+        "21888242871839275222246405745257275088548364400416034343698204186575808495617",
+    )
+    .unwrap();
+
+    for i in 0..8 {
+        let coeff = ((i * 7) % 8);
+        den2 = h0w8[0 + coeff];
+        // println!("den2: {}", den2);
+        den3 = y.add(q.sub(h0w8[0 + (i)]));
+        // println!("den3: {}", den3);
+
+        li_s0_inv[i] = den1.mul(den2).mul(den3);
+
+        // println!("li_s0_inv: {}", li_s0_inv[i]);
+        // println!();
+    }
+    // println!("li_s0_inv: {}", li_s0_inv[7]);
+
+    li_s0_inv
+}
+
+pub fn computeLiS1(
+    y: Fp256<FrParameters>,
+    h1w4: Vec<Fp256<FrParameters>>,
+) -> [Fp256<FrParameters>; 4] {
+    let root0 = h1w4[0];
+    let mut den1 = Fr::from_str("1").unwrap();
+    den1 = den1.mul(root0).mul(root0);
+
+    den1 = den1.mul(Fr::from_str("4").unwrap());
+
+    let q = Fr::from_str(
+        "21888242871839275222246405745257275088548364400416034343698204186575808495617",
+    )
+    .unwrap();
+
+    let mut den2;
+    let mut den3;
+
+    let mut li_s1_inv: [Fp256<FrParameters>; 4] = [Fr::zero(); 4];
+
+    for i in 0..4 {
+        let coeff = ((i * 3) % 4);
+        den2 = h1w4[0 + coeff];
+        den3 = y.add(q.sub(h1w4[0 + (i)]));
+        li_s1_inv[i] = den1.mul(den2).mul(den3);
+    }
+
+    // println!("li_s1_inv: {}", li_s1_inv[3]);
+    li_s1_inv
+}
+
+pub fn computeLiS2(
+    y: Fp256<FrParameters>,
+    xi: Fp256<FrParameters>,
+    h2w3: Vec<Fp256<FrParameters>>,
+    h3w3: Vec<Fp256<FrParameters>>,
+) -> [Fp256<FrParameters>; 6] {
+    let q = Fr::from_str(
+        "21888242871839275222246405745257275088548364400416034343698204186575808495617",
+    )
+    .unwrap();
+
+    // let den1 := mulmod(mulmod(3,mload(add(pMem, pH2w3_0)),q), addmod(mload(add(pMem, pXi)) ,mod(sub(q, mulmod(mload(add(pMem, pXi)), w1 ,q)), q), q), q)
+    let omegas = get_omegas();
+    let mut den1 = (Fr::from_str("3").unwrap().mul(h2w3[0])).mul(xi.add(q.sub(xi.mul(omegas.w1))));
+
+    let mut den2;
+    let mut den3;
+
+    let mut li_s2_inv: [Fp256<FrParameters>; 6] = [Fr::zero(); 6];
+
+    for i in 0..3 {
+        let coeff = ((i * 2) % 3);
+        den2 = h2w3[0 + coeff];
+        den3 = y.add(q.sub(h2w3[0 + (i)]));
+        li_s2_inv[i] = den1.mul(den2).mul(den3);
+    }
+
+    den1 = (Fr::from_str("3").unwrap().mul(h3w3[0])).mul(xi.mul(omegas.w1).add(q.sub(xi)));
+
+    for i in 0..3 {
+        let coeff = ((i * 2) % 3);
+        den2 = h3w3[0 + coeff];
+        den3 = y.add(q.sub(h3w3[0 + (i)]));
+        li_s2_inv[i + 3] = den1.mul(den2).mul(den3);
+    }
+
+    li_s2_inv
+}
+
+pub fn inverseArray(
+    denH1: Fp256<FrParameters>,
+    denH2: Fp256<FrParameters>,
+    zhInv: Fp256<FrParameters>,
+    li_s0_inv: [Fp256<FrParameters>; 8],
+    li_s1_inv: [Fp256<FrParameters>; 4],
+    li_s2_inv: [Fp256<FrParameters>; 6],
+    eval_l1: &mut Fp256<FrParameters>,
+) -> (LISValues, Fp256<FrParameters>, Fp256<FrParameters>) {
+    // let mut local_eval_l1 = eval_l1.clone();
+    let mut local_den_h1 = denH1.clone();
+    let mut local_den_h2 = denH2.clone();
+    let mut local_zh_inv = zhInv.clone();
+    let mut local_li_s0_inv = li_s0_inv.clone();
+    let mut local_li_s1_inv = li_s1_inv.clone();
+    let mut local_li_s2_inv = li_s2_inv.clone();
+
+    let mut _acc: Vec<Fp256<FrParameters>> = Vec::new();
+
+    _acc.push(zhInv.clone());
+
+    let mut acc = zhInv.mul(denH1);
+    _acc.push(acc.clone());
+
+    acc = acc.mul(denH2);
+    _acc.push(acc.clone());
+
+    for i in 0..8 {
+        acc = acc.mul(local_li_s0_inv[i]);
+        _acc.push(acc);
+    }
+    for i in 0..4 {
+        acc = acc.mul(local_li_s1_inv[i]);
+        _acc.push(acc);
+    }
+    for i in 0..6 {
+        acc = acc.mul(local_li_s2_inv[i]);
+        _acc.push(acc);
+    }
+    acc = acc.mul(eval_l1.clone());
+    _acc.push(acc);
+    // println!("acc: {}", acc);
+    // println!("acc wala xeval_l1: {}", eval_l1);
+
+    let mut inv = get_proof().eval_inv;
+
+    // println!("inv: {}", inv);
+
+    let check = inv.mul(acc);
+    // println!("check: {}", check);
+    assert!(check == Fr::one());
+
+    acc = inv.clone();
+
+    _acc.pop();
+    inv = acc.mul(_acc.last().unwrap().clone());
+    acc = acc.mul(eval_l1.clone());
+    *eval_l1 = inv;
+    // println!("herer eval_l1: {}", eval_l1);
+
+    for i in (0..6).rev() {
+        _acc.pop();
+        inv = acc.mul(_acc.last().unwrap().clone());
+        acc = acc.mul(local_li_s2_inv[i]);
+        local_li_s2_inv[i] = inv;
+    }
+    // println!("local_li_s2_inv_0: {}", local_li_s2_inv[0]);
+
+    for i in (0..4).rev() {
+        _acc.pop();
+        inv = acc.mul(_acc.last().unwrap().clone());
+        acc = acc.mul(local_li_s1_inv[i]);
+        local_li_s1_inv[i] = inv;
+    }
+
+    // println!("local_li_s1_inv_0: {}", local_li_s1_inv[0]);
+
+    for i in (0..8).rev() {
+        _acc.pop();
+        inv = acc.mul(_acc.last().unwrap().clone());
+        acc = acc.mul(local_li_s0_inv[i]);
+        local_li_s0_inv[i] = inv;
+    }
+
+    // println!("local_li_s0_inv_0: {}", local_li_s0_inv[0]);
+
+    _acc.pop();
+    inv = acc.mul(_acc.last().unwrap().clone());
+    acc = acc.mul(denH2);
+    local_den_h2 = inv;
+
+    _acc.pop();
+    inv = acc.mul(_acc.last().unwrap().clone());
+    acc = acc.mul(denH1);
+    local_den_h1 = inv;
+
+    local_zh_inv = acc;
+
+    let lis_values = LISValues {
+        li_s0_inv: local_li_s0_inv,
+        li_s1_inv: local_li_s1_inv,
+        li_s2_inv: local_li_s2_inv,
+    };
+
+    (lis_values, local_den_h1, local_den_h2)
+    // println!("local_zh_inv: {}", local_zh_inv);
+}
