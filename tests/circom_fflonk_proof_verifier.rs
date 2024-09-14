0d3e5ee150e55f46c08568761bf89db8078ca1d3
Paul Cheng <chengyuejia@foxmail.com>
2024-09-14 09:44:35 +0800

    feat: add snarkjs fflonk keccak  proof

--- tests/circom_fflonk_proof_verifier.rs
+++ tests/circom_fflonk_proof_verifier.rs
@@ -2,8 +2,7 @@ use ark_fflonk_verifier::proof::Proof;
 use ark_fflonk_verifier::utils::load_public_input;
 use ark_fflonk_verifier::verifier::fflonk_verifier;
 use ark_fflonk_verifier::vk::{SnarkJSVK, VerificationKey};
-use std::fs::File;
-use std::io::Read;
+
 
 #[test]
 fn circom_fflonk_proof_verifier() {
@@ -11,8 +10,7 @@ fn circom_fflonk_proof_verifier() {
     let circom_file_path = current_dir.join("resources/circom/");
 
     let public_file = circom_file_path.join("public.json");
-    // let vk_file = circom_file_path.join("verification_key.json");
-    let vk_file = circom_file_path.join("zksync_vk.json");
+    let vk_file = circom_file_path.join("verification_key.json");
     let proof_file = circom_file_path.join("proof.json");
 
     let snarkjs_vk = SnarkJSVK::load(vk_file).unwrap();
